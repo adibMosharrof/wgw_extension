f4b0ccf016f90059a22bbfe6d5836d2f61940bd1
Adib Mosharrof <amo304@uky.edu>
2022-02-17 21:18:06 -0500

    cleaned data loader, using model in gpu

--- dataset.py
+++ dataset.py
@@ -16,38 +16,37 @@ import torchvision.transforms.functional as F
 from torchvision.utils import draw_bounding_boxes
 from collections import Counter
 from torchvision.io import read_image
+import csv
 
 class ObjectDataModule(pl.LightningDataModule):
     def __init__(
         self,
         cvusa_root:str,
-        batch_size:int=32,
-        num_workers:int=0
+        batch_size:int=1,
+        num_workers:int=2,
+        index:int=0,
+        num_items:int=32
     ):
         super().__init__()
         self.batch_size = batch_size
         self.cvusa_root = cvusa_root
         self.num_workers = num_workers
+        self.index = index
+        self.num_items = num_items
     
     def setup(self, stage=None):
+        start = self.index*self.num_items
         img_names = list(map(
             Path,
             open(
                 self.cvusa_root / "flickr_images.txt"
-            ).read().strip().split("\n")[35:50],
+            ).read().strip().split("\n")[start:start+self.num_items],
         ))
-        model = fasterrcnn_resnet50_fpn(pretrained=True, progress=False)
-        model = model.eval()
         self.tfm = transforms.Compose([
             T.Resize([537,936])
         ])
-        self.obj_dataset = ObjectDataset(img_names=img_names, cvusa_root=self.cvusa_root, model=model, tfms=self.tfm)
-        dl = self.train_dataloader()
-        results = []
-        # for i, batch in enumerate(self.obj_dataset):
-        for i, batch in enumerate(self.train_dataloader()):
-            results.append(batch)
-        #write results to csv file
+        self.obj_dataset = ObjectDataset(img_names=img_names, cvusa_root=self.cvusa_root, tfms=self.tfm)
+
 
     def train_dataloader(self):
         return DataLoader(
@@ -58,38 +57,34 @@ class ObjectDataModule(pl.LightningDataModule):
             shuffle=False,
             drop_last=True
         )
+    
+    def write_csv(self, results):
+        headers = ['image_id','labels']
+        with open(f'out/object_detection_{self.num_items}_{self.index}.csv', 'w', encoding='UTF8', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=headers)
+            writer.writeheader()
+            writer.writerows(results)
 
 class ObjectDataset(Dataset):
-    def __init__(self, cvusa_root:str, img_names, model=None, tfms=None) -> None:
+    def __init__(self, cvusa_root:str, img_names, tfms=None) -> None:
         super().__init__()
         self.img_names = img_names
         self.cvusa_root = cvusa_root
-        self.model = model
         self.tfms = tfms
         self.score_threshold = 0.8
         self.out_path = Path("out/")
     
     def __getitem__(self, index):
-        # if index >= len(self):
-        #     raise IndexError("index greater than data length")
+        if index >= len(self) or index <0:
+            raise IndexError("index greater than data length")
         img_name = self.img_names[index]
         img = read_image(str(self.cvusa_root / img_name))
         if self.tfms:
             img = self.tfms(img)
-        batch_int = torch.stack([img])
-        batch = convert_image_dtype(batch_int, dtype=torch.float)
-        output = self.model(batch)[0]
-        mask = output['scores'] > self.score_threshold
-        img_bbox = draw_bounding_boxes(batch_int[0], boxes=output['boxes'][mask], width=4)
-        labels_idx = output['labels'][mask]
-        img = F.to_pil_image(img_bbox)
-        bbox_path = self.out_path/f'bbox_{img_name.name}'
-        img.save(bbox_path)
-        labels = Counter(labels_idx.tolist())
+        img = convert_image_dtype(img)
         row = {
-            'labels': {**labels},
             'image_id': img_name.name,
-            'bbox_path': str(bbox_path)
+            'image': img
         }
         return row
 
--- run.py
+++ run.py
@@ -1,13 +1,46 @@
 from dataset import ObjectDataModule, ObjectDataset
 import pytorch_lightning as pl
 from pathlib import Path
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from torchvision.transforms.functional import convert_image_dtype
+import csv
+from collections import Counter
+import torch
+
+def write_csv(headers, results):
+		with open(f'out/object_detection.csv', 'w', encoding='UTF8', newline='') as f:
+			csvwriter = csv.writer(f) 
+			csvwriter.writerow(headers) 
+			csvwriter.writerows(results)
+		
 
 if __name__ == "__main__":
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 	dm = ObjectDataModule(
-		cvusa_root = Path("/u/eag-d1/data/crossview/cvusa/")
-
+		cvusa_root = Path("/u/eag-d1/data/crossview/cvusa/"),
+		index=0
 	)
 	dm.setup()
-
-	# for i, batch in enumerate(dm.train_dataloader()):
-	# 	a=1
+	score_threshold = 0.8
+	num_labels = 91
+	model = fasterrcnn_resnet50_fpn(pretrained=True, progress=False)
+	model = model.to(device)
+	model = model.eval()
+	results = []
+	for batch in dm.train_dataloader():
+		
+		# output = model(batch['image'])
+		d = batch['image'].to(device)
+		output = model(d)
+		for out, img_id in zip(output, batch['image_id']):
+			all_labels = [""]*num_labels
+			mask = out['scores'] > score_threshold
+			labels_idx = out['labels'][mask]
+			labels = Counter(labels_idx.tolist())
+			for index, count in labels.items():
+				all_labels[index] = count	
+			all_labels.insert(0,img_id)
+			results.append(all_labels)
+	headers = ['image_id']
+	headers.extend(list(range(num_labels)))
+	write_csv(headers, results)
